e9747e22a3740e70724e92dcd5cc9fe7c9c50825
Rohan Agarwal <agarwalrohan2004@gmail.com>
2024-01-18 20:16:12 +0530

    18

---
+++ SumOfDigits.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int sumOfDigits = 0;
+
+        //int original_n = n;
+
+        while(n > 0)
+        {
+            sumOfDigits += n % 10;
+            
+            n = n/10;
+        }
+
+        System.out.println("Sum of digits is " + sumOfDigits);
+
+        sc.close();
+    }
+}
